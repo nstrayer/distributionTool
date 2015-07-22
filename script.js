474003d34227103bad9dd3acc39bed87ab6476c3
Nick Strayer <nstrayer@uvm.edu>
2015-07-21 22:11:52 -0700

    Sliders generation work is done. Now moving on to generalizing the code for arbitrary distributions.

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-//just some nonsense to move objects to front easily.
-d3.selection.prototype.moveToFront = function() {
-  return this.each(function(){
-    this.parentNode.appendChild(this);
-  });
-};
+// Script for a simple tool built using only d3 to help users interact with, explore and generally
+// appreciate the world of probability distributions.
+// Made by Nick Strayer.
 
-var theDistributions = [
-    {"name": "logistic", "equation": logistic, "parameters": ["mu", "theta"], "starting": [0.5, 0.3]}
-]
+//Where we store all the neccesary info about the given distributions.
+var theDistributions = {
+    "logistic": {"equation"  : logistic, //this needs to be defined in the code.
+                 "parameters": ["mu", "theta"], //names of the parameters
+                 "starting"  : [0.5, 0.3], //what we start them out at.
+                 "plusMinus" : [.4, .2]} //how far around the starting values we can go.
+}
 
-var width = parseInt(d3.select("#viz").style("width").slice(0, -2)),
+// ----------------------------------------------------------------------------------------
+// All the usual d3 setup stuff.
+// ----------------------------------------------------------------------------------------
+
+var width  = parseInt(d3.select("#viz").style("width").slice(0, -2)),
     height = $(window).height() - 150;
 
 var svg = d3.select("#viz").append("svg")
     .attr("height", height)
     .attr("width", width)
-
-//variables for the logistic distribtion
-var theta = 0.5,
-    mu    = 0.4;
-
-//Logistic Distributions
-var logistic = function(x, m, t) {
-    // var mu = 0;
-    var y =  (1 / (Math.sqrt(2 * Math.PI) * t)) * (1 / x) *
-        Math.exp(-Math.pow((Math.log(x) - m), 2) / (2 * Math.pow(t, 2)));
-    return y;
-}
-
-var normal = function(x, m, sd){
-    return (1 / (sd * Math.sqrt(2*Math.PI))) * (Math.exp( -(Math.pow(x - m, 2))/(2*sd*sd)  ))
-}
 
 var x = d3.scale.linear()
     .domain([0, 5])
@@ -45,12 +34,57 @@ var line = d3.svg.line()
     .x(function(d) { return x(d.x); })
     .y(function(d) { return y(d.y); });
 
+//variables for the logistic distribtion this stuff will need to go soon.
+var theta = 0.5,
+    mu    = 0.4,
+    params; //initialize the vector that holds the parameters for a given distribution.
+
+// ----------------------------------------------------------------------------------------
+// Where all the distribution functions go.
+// ----------------------------------------------------------------------------------------
+
+//Logistic Distribution
+var logistic = function(x, m, t) {
+    // var mu = 0;
+    var y =  (1 / (Math.sqrt(2 * Math.PI) * t)) * (1 / x) *
+        Math.exp(-Math.pow((Math.log(x) - m), 2) / (2 * Math.pow(t, 2)));
+    return y;
+}
+
+// //Logistic Distribution, new version.
+// var logistic = function(x, params) {
+//
+//     var mu    = params[0],
+//         theta = params[1];
+//
+//     var y =  (1 / (Math.sqrt(2 * Math.PI) * t)) * (1 / x) *
+//         Math.exp(-Math.pow((Math.log(x) - m), 2) / (2 * Math.pow(t, 2)));
+//     return y;
+// }
+
+//Normal or Gaussian Distribution.
+var normal = function(x, params){
+
+    var m  = params[0],
+        sd = params[1];
+
+    return (1 / (sd * Math.sqrt(2*Math.PI))) * (Math.exp( -(Math.pow(x - m, 2))/(2*sd*sd)  ))
+}
+
+//Function to generate the lines. This will need to be changed to take arbitrary params.
 function aLine(xVal, equation, m, t){
     console.log("t " + t + ", m " + m)
     return _.map(xVal, function(x) {
         return { "x": x, "y": equation(x, m, t) }
  })
 }
+
+// //new version
+// function aLine(xVal, equation, params){
+//     return _.map(xVal, function(x) {
+//         return { "x": x, "y": equation(x,params) }
+//  })
+// }
 
 function updateLine(x, equation, m, t){
     mu = m
@@ -82,6 +116,7 @@ svg.selectAll(".distribution")
 
 //Work on getting the sliders to automatically generate:
 
+//Generates a single slider.
 function makeSlider(name, val, low, high, onInput){
 
     var div = d3.select("#menu")
@@ -101,12 +136,18 @@ function makeSlider(name, val, low, high, onInput){
         .attr("step", (high - low)/100)
         .attr("oninput", onInput)
 }
-var params;
+
 var sampleParams = [
     {"name": "Alpha", "startVal": 1,  "slideLow": .1, "slideHigh":4 },
     {"name": "Beta" , "startVal": 2,  "slideLow": .2, "slideHigh":5 },
     {"name": "Gamma", "startVal": 3,  "slideLow": .3, "slideHigh":6 }
 ]
+
+var logisticParams = [
+    {"name": "Theta", "startVal": 1,  "slideLow": .1, "slideHigh":4 },
+    {"name": "Mu" , "startVal": 2,  "slideLow": .2, "slideHigh":5 }
+]
+
 
 // generates the string that goes into the onInput attribute of the input slider.
 function makeOnInput(params, equation, loc){
@@ -121,4 +162,15 @@ function makeOnInput(params, equation, loc){
     }
     call = call + ")"
     return call;
+}
+
+//runs through the parameters and takes the function name to generate the sliders for a given distribution.
+function drawSliders(params, functionName){
+    params.forEach(function(d,i){
+        makeSlider(d.name, d.startVal, d.slideLow, d.slideHigh, makeOnInput(params, functionName, i))
+    })
+}
+
+function initializeDist(dist){
+    var foo = "bar"
 }
